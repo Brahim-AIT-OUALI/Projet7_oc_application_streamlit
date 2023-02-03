00518328eec860ee4b5c3772515c567aaa100fc8
Brahim-AIT-OUALI <115220907+Brahim-AIT-OUALI@users.noreply.github.com>
2023-02-04 00:31:21 +0100

    Update Appli_p7.py

--- Appli_p7.py
+++ Appli_p7.py
@@ -33,7 +33,7 @@ def numeric(col):
 def relation_entre_variables (var1, var2):
     fig = plt.figure(figsize=(12,5))
     plt.title(var2 +" en fonction de " + var1)
-    sns.scater(data=X, x=var1, y=var2)
+    sns.scatterplot(data=X, x=var1, y=var2)
     st.pyplot(fig)
         
 
