960ee4db13cb7417e2832eb009fe86dbfa440a13
Brahim-AIT-OUALI <115220907+Brahim-AIT-OUALI@users.noreply.github.com>
2023-02-06 17:33:05 +0100

    Update Appli_p7.py

--- Appli_p7.py
+++ Appli_p7.py
@@ -84,7 +84,7 @@ if __name__=="__main__":
 
     numeric("DAYS_EMPLOYED")
     st.write("Valeur pour le client = ", donnees_client["DAYS_EMPLOYED"].iloc[0])
-    st.writemax(X[col].tolist()))
+    st.write(max(X[col].tolist()))
         
     relation_entre_variables ("DAYS_BIRTH", "DAYS_EMPLOYED")
 
