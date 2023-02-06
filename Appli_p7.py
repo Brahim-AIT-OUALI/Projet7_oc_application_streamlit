7740382b726cb539afbab10e22a2046118084268
Brahim-AIT-OUALI <115220907+Brahim-AIT-OUALI@users.noreply.github.com>
2023-02-06 16:21:48 +0100

    Update Appli_p7.py

--- Appli_p7.py
+++ Appli_p7.py
@@ -28,6 +28,8 @@ def numeric(col):
     fig = plt.figure(figsize=(12,5))
     plt.title("Distribution of "+col)
     ax = sns.distplot(X[col])
+    X1 = X[[col; 'SK_ID_CURR']]
+    plt.axline((X1[X1['SK_ID_CURR']==input_df], 0), (X1[X1['SK_ID_CURR']==input_df], 1), c='darkorange', ls='dashed')
     st.pyplot(fig)
         
 def relation_entre_variables (var1, var2):
