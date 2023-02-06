9895a360ce69edd339fa7d3f72b012527d97cf23
Brahim-AIT-OUALI <115220907+Brahim-AIT-OUALI@users.noreply.github.com>
2023-02-06 16:34:54 +0100

    Update Appli_p7.py

--- Appli_p7.py
+++ Appli_p7.py
@@ -28,6 +28,7 @@ def numeric(col):
     fig = plt.figure(figsize=(12,5))
     plt.title("Distribution of "+col)
     ax = sns.distplot(X[col])
+    ax = sns.distplot(donnees_client[col])
     st.pyplot(fig)
         
 def relation_entre_variables (var1, var2):
