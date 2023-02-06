f101666b483aa50a0a73c95bbbc4f3cad816032f
Brahim-AIT-OUALI <115220907+Brahim-AIT-OUALI@users.noreply.github.com>
2023-02-06 16:28:42 +0100

    Update Appli_p7.py

--- Appli_p7.py
+++ Appli_p7.py
@@ -29,7 +29,7 @@ def numeric(col):
     plt.title("Distribution of "+col)
     ax = sns.distplot(X[col])
     X1 = X[[col, 'SK_ID_CURR']]
-    sns.distplot((X1[X1['SK_ID_CURR']==input_df].iloc[0,0], 0), (X1[X1['SK_ID_CURR']==input_df].iloc[0,0], 1), c='darkorange', ls='dashed')
+    sns.distplot((X1[X1['SK_ID_CURR']==input_df].iloc[0,0]), (X1[X1['SK_ID_CURR']==input_df].iloc[0,0]), c='darkorange', ls='dashed')
     st.pyplot(fig)
         
 def relation_entre_variables (var1, var2):
