ee1a31e6bea5aff2e92bb7a31649f2f64424cc1b
Brahim-AIT-OUALI <115220907+Brahim-AIT-OUALI@users.noreply.github.com>
2023-02-08 00:17:05 +0100

    Update Appli_p7.py

--- Appli_p7.py
+++ Appli_p7.py
@@ -143,7 +143,7 @@ if __name__=="__main__":
 
     st.subheader("3. Résultat pour ce client ")
    
-    st.write("P = ", prevision["response"])
+    st.write("P = ", prevision)
     seuil = 0.3783783783783784
     
     
