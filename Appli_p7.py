cee2638af223503e9db1b20ca04304b6a9646e70
Brahim-AIT-OUALI <115220907+Brahim-AIT-OUALI@users.noreply.github.com>
2023-02-07 21:48:32 +0100

    Update Appli_p7.py

--- Appli_p7.py
+++ Appli_p7.py
@@ -113,7 +113,7 @@ if __name__=="__main__":
         
     st.write(data)
     # sending post request and saving response as response object
-    r = requests.post(url = API_ENDPOINT, data = input_df)
+    r = requests.post(url = API_ENDPOINT, data = data)
     #r = requests.post(url = API_ENDPOINT, data = json.dumps(data))
     prevision = r.tex
     st.write(prevision)  
